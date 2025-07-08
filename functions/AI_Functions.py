dcaa77c1ac83ccb0e168b12ad8ac1754f81dcf7d
AjithNair-cyber <nairajith1507@gmail.com>
2025-07-08 16:49:08 +0530

    added hardcoded values for checking

--- functions/AI_Functions.py
+++ functions/AI_Functions.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") or "AIzaSyAswb8zCjcotAY9EkfDfpvP_BanFk0ui0c"
 
 LLM = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
