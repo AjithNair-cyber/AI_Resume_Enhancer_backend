219e3a991f8a90b14db63ee02a39dad9571b61f5
AjithNair-cyber <nairajith1507@gmail.com>
2025-08-08 21:22:17 +0530

    Removed status from clean json output

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ def upload_resume():
         extracted_resume = extract_text_from_pdf(filepath)
         job_description = request.form.get('job')
         enhanced_resume = generate_enhanced_resume(job_description, extracted_resume)
-        data =  clean_json_output(enhanced_resume), 200
+        data =  clean_json_output(enhanced_resume)
         return success_response_formatter(json.loads(data))
         
 @app.route("/hello", methods=["GET"])
