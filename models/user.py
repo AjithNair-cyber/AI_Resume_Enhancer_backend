0f10ab233490e7165a3c821778e684b9dd9907ac
AjithNair-cyber <nairajith1507@gmail.com>
2025-08-13 13:43:55 +0530

    made schema changes to hold created_date

--- models/user.py
+++ models/user.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timezone
+
 class User :
     def __init__(self, email, password, name):
         self.email = email
         self.password = password
         self.name = name
+        self.created_at = datetime.now(timezone.utc)
 
     def getUser(self) :
-        return {"email" : self.email, "password" : self.password, "name" : self.name}
+        return {"email" : self.email, "password" : self.password, "name" : self.name, "created_at" : self.created_at}
